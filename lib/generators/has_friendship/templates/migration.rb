deace53d302330fea331e58f2345ec2a62d5616e
Hugo Villero <hugo.villero@gmail.com>
2015-07-25 19:17:40 -0400

    Fix spelling mistake on requestor / requester

--- lib/generators/has_friendship/templates/migration.rb
+++ lib/generators/has_friendship/templates/migration.rb
@@ -3,7 +3,7 @@ class CreateFriendships < ActiveRecord::Migration
     create_table :friendships do |t|
       t.references :friendable, polymorphic: true
       t.integer  :friend_id
-      t.integer  :requestor_id
+      t.integer  :requester_id
       t.integer  :approver_id
       t.string   :status
       t.string   :connection_type
